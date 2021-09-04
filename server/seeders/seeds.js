add9688374bda468a01be63dc25e5de3b47b1a43
Naomi Valdovinos <n.valdovinos74@gmail.com>
2021-09-04 10:45:32 -0400

    added seeders files

---
+++ server/seeders/seeds.js
@@ -0,0 +1,31 @@
+const db = require('../config/connection');
+const {User, Project, Tasks} = require('../models');
+const userSeeds = require('./userSeeds.json');
+const projectSeeds = require('./projectSeeds.json');
+
+db.once('open', async () => {
+    try {
+      await Project.deleteMany({});
+      await User.deleteMany({});
+  
+      await User.create(userSeeds);
+  
+      for (let i = 0; i < projectSeeds.length; i++) {
+        const { _id, projectAuthor } = await Project.create(projectSeeds[i]);
+        const user = await User.findOneAndUpdate(
+          { username: projectAuthor },
+          {
+            $addToSet: {
+              thoughts: _id,
+            },
+          }
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  
+    console.log('all done!');
+    process.exit(0);
+  });
