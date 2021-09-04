b4bc5b3bf31ff03396a69742d9830e8989043b08
Savannah Kolb <savannahnkolb@gmail.com>
2021-09-04 13:54:41 -0400

    added task query in resolvers and updated in typeDefs to reflect

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,6 +23,25 @@ const resolvers = {
         return Project.findOne({ _id: projectId });
     },
 
+    //I am not sure if I set these up correctly. I may be over complicating but 
+      // we have mulitple tasks for each project, 
+        // & we have muliple tasks for each user associated with the project
+          // & we have tasks assigned by user
+
+    tasks: async (parent, {taskID}) => {
+      return Tasks.findOne({_id: taskID});
+    },
+
+    userTasks: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Tasks.find(params)
+      },
+
+    projectTasks: async (parent, {projectID}) => {
+      const params = projectID ? { projectID } : {};
+      return Tasks.find(params)
+    },
+    
     me: async (parent, args, context) => {
     if (context.user) {
     return User.findOne({ _id: context.user._id }).populate('thoughts');
