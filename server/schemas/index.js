5358dc9a4e5f6a2346390547ec3b81b829ab422e
Savannah Kolb <savannahnkolb@gmail.com>
2021-09-04 10:43:32 -0400

    fixed apollo error message

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,6 +1,4 @@
-const {ApolloServer} = require('apollo-server')
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({typeDefs})
 module.exports = {typeDefs,resolvers};
