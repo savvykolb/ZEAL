b5acb92e5e116e8469e1a3c1322c3a27bcd13a95
Naomi969 <79178682+Naomi969@users.noreply.github.com>
2021-09-05 19:39:16 -0400

    Update LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
-
+import {Link} from "react-router-dom";
 import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutation";
