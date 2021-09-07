8e6dfef158b8ec1f2848041cf2bb370b54ab407d
Naomi Valdovinos <n.valdovinos74@gmail.com>
2021-09-07 16:47:04 -0400

    saving

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -10,22 +10,8 @@ db.once('open', async () => {
       await User.deleteMany({});
       await User.create(userSeeds);
   
-<<<<<<< HEAD
-      for (let i = 0; i < projectSeeds.length; i++) {
-        const { _id, projectAuthor } = await Project.create(projectSeeds[i]);
-        const user = await User.findOneAndUpdate(
-          { username: projectAuthor },
-          {
-            $addToSet: {
-              projects: _id,
-            },
-          }
-        );
-      }
-=======
       console.log("well well well")
       process.exit(0);
->>>>>>> main
     } catch (err) {
       throw err;
     }
