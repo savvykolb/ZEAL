95236fafb37e3474d783eb316d4a554fcbc6018d
Savannah Kolb <savannahnkolb@gmail.com>
2021-09-11 00:33:46 -0400

    updated home

--- client/src/pages/Homescreen.js
+++ client/src/pages/Homescreen.js
@@ -1,54 +1,68 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from "react-router-dom"
+import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom"
+
 function Homescreen() {
-    const [projects, setProjects] = useState([])
-//dependent array line 9...empty array
-    useEffect(()=>{
-const projectsFromStorage = localStorage.getItem("projectList")
-console.log(projectsFromStorage)
-if (projectsFromStorage) {
-  setProjects(JSON.parse(projectsFromStorage))
-}
-    },[])
+  const [projects, setProjects] = useState([])
+  const [users, setUsers] = useState([])
+  //dependent array line 9...empty array
+  useEffect(() => {
+    const projectsFromStorage = localStorage.getItem("projectList")
+    console.log(projectsFromStorage)
+    if (projectsFromStorage) {
+      setProjects(JSON.parse(projectsFromStorage))
+    }
+  }, [])
+
+  useEffect(() => {
+    const usersFromStorage = localStorage.getItem("zealLoggedIn")
+    console.log(usersFromStorage)
+    if (usersFromStorage) {
+      setUsers(JSON.parse(usersFromStorage))
+    }
+  }, [])
 
 
 
 
   return (
     <div>
-      <div className="d-flex min-vh-100">
-        <div className="container-fluid my-auto d-flex flex-column w-75">
-          <h1 id ="home-title" className="display-3 text-center">Your Projects</h1>
-          <div className="row bg-dark ">
-       
-             
-                <div className="col-12 col-md-6 col-lg-4 ">
-                  <i className="fas fa-plus"></i>
-                  <i className="bi bi-plus"></i>
+      <section className="d-flex flex-column align-items-center justify-content-center title" >
+        <h1>Welcome Back  <span id="zeal"> {users.username}</span>!</h1>
+        <h2> Please login to you account.</h2>
 
-                  <Link to="/create" className="btn btn-block p-3 my-2 btn-secondary">
-                    <span className="plus">&#43;</span>
-                    <h3>Add Projects</h3>
-                  </Link>
-                </div>
-                {projects.map((project, i )=>{
-                    return <div key = {i} className="col-12 col-md-6 col-lg-4 bg-secondary m-2">
-               <h3>{project.name}</h3>
-                  <h5>{project.dueDate}</h5>
-                  <p>{project.description}</p>
-                </div>
-                })}
-           
-          
-            
+        <div className="row container ">
 
-            
-          </div>
+
+        <div >
+        <i className="fas fa-plus"></i>
+        <i className="bi bi-plus"></i>
+
+        <Link to="/create" className="btn btn-block p-3 my-2 btn-secondary" id="addProject">
+        <span className="plus">&#43; </span>
+        <h3>Add Projects</h3>
+        </Link>
         </div>
+      {projects.map((project, i) => {
+        return (
+          <div className="row container" key = {i}>
+        <Card style={{ width: '100rem' }}>
+        <Card.Header className="d-flex flex-column align-items-center justify-content-center projectTitle">{project.name}</Card.Header>
+      <br></br>
+        <Card.Text className="d-flex flex-column align-items-center justify-content-center">
+      {project.description}
+        </Card.Text>
+        <Card.Text className="d-flex flex-column align-items-center justify-content-center">
+      {project.dueDate}
+        </Card.Text>
+        </Card>
       </div>
-    </div>
-  )
-}
+        )},
+      )},
+        </div>
+        </section>
+  </div>
+  )}
 
 export default Homescreen;
